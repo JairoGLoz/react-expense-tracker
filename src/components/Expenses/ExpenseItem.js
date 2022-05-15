dfa3d55a6b34977ea0022d3fc6d35aaf3111ee51
Jairo Lozano <jairo25492@gmail.com>
2022-05-15 12:34:45 -0600

    Start using state on components

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+
+  // useState always returns an array of two elements
+  //
+  // title is a pointer to props.title
+  // setTitle is a setter function that we must use to 
+  // change title value.
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    // by using setTitle,
+    // title will receive a new value and that change will
+    // cause the component function to be executed again.
+    setTitle("update");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
